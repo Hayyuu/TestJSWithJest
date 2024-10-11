36a23fd02bd164b63b687656243eba1c81cc8639
Hayyuu <hayatahimed518@gmail.com>
2024-10-11 18:02:33 +0300

    feat : add calculator object along its methods to make the tests pass

---
+++ calculator.js
@@ -0,0 +1,9 @@
+class Calculator{
+    add = (num1,num2) => num1+num2;
+    subtruct = (num1,num2) => num1-num2;
+    multiply = (num1,num2) => num1*num2;
+    divide = (num1,num2) =>   num1/num2
+
+}
+
+export {Calculator}
--- calculator.test.js
+++ calculator.test.js
@@ -1,12 +1,15 @@
+import { Calculator } from "./calculator";
+let calculator=new Calculator();
+
 test('Add 2 to 3 to be 5',()=>{
     expect(calculator.add(2,3)).toBe(5);
 });
 test('Subtruct 2 from 3 to be -1',()=>{
-    expect(calculator.subtruct(2,3)).toBe(5);
+    expect(calculator.subtruct(2,3)).toBe(-1);
 });
 test('Multiply 2 with 3 to be 6',()=>{
-    expect(calculator.multiply(2,3)).toBe(5);
+    expect(calculator.multiply(2,3)).toBe(6);
 });
 test('divide 2 to 0 to be NAN',()=>{
-    expect(calculator.divide(2,3)).toBe(5);
+    expect(calculator.divide(2,0)).toBe(Infinity);
 });
