5a9c5feaf7c0195c78982d9cb4341fa181ffcb25
Hayyuu <hayatahimed518@gmail.com>
2024-10-11 17:44:09 +0300

    test: add a test for calculator object

---
+++ calculator.test.js
@@ -0,0 +1,12 @@
+test('Add 2 to 3 to be 5',()=>{
+    expect(calculator.add(2,3)).toBe(5);
+});
+test('Subtruct 2 from 3 to be -1',()=>{
+    expect(calculator.subtruct(2,3)).toBe(5);
+});
+test('Multiply 2 with 3 to be 6',()=>{
+    expect(calculator.multiply(2,3)).toBe(5);
+});
+test('divide 2 to 0 to be NAN',()=>{
+    expect(calculator.divide(2,3)).toBe(5);
+});
